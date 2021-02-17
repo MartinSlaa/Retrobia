743be9decf54901fa94dd33ce08ce9be34abdb97
Lasse <lalundstad@yahoo.com>
2021-02-17 14:31:54 +0100

    updated music and code.

--- Side_Step/Side_Step_main.py
+++ Side_Step/Side_Step_main.py
@@ -24,3 +24,62 @@ def StandByForPlayerToPressKey():
                 terminate()
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE: #ESC quits the game
+                    terminate()
+                return
+
+def playerHasHitVillain(playerRect, villains):
+    for v in villains:
+        if playerRect.colliderect(v['rect']):
+            return True
+        return False
+
+def provideText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, Y)
+    surface.blit(textobj, textrect)
+
+#Set up pygame, window and the mouse cursor
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('SIDE-STEP')
+pygame.mouse.set_visible(False)
+
+#FONTS
+font = pygame.font.SysFont(None, 48)
+
+#SOUNDS
+gameOverSound = pygame.mixer.Sound('gameover.wav')
+pygame.mixer.music.load('background.mp3')
+
+#IMAGES
+playerImage = pygame.image.load('player.png')
+PlayerRect = playerImage.get_rect()
+villainImage = pygame.image.load('villain.png')
+
+#Start Screen
+windowSurface.fill(BACKGROUNDCOLOR)
+drawText('SIDE-STEP', font, windowSurface, (WINDOWWIDTH / 3),
+    WINDOWHEIGHT / 3))
+drawText ('Press any key to start!', font, windowSurface,
+    (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3) + 50)
+pygame.display.update()
+StandByForPlayerToPressKey()
+
+topScore = 0
+while True:
+    # Setup menu before game start
+    villains = []
+    score = 0
+    playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
+    moveLeft = moveRight = moveUp = moveDown = False
+    reverseCheat = slowCheat = False
+    villainAddCounter = 0
+    pygame.mixer.music.play(-1, 0.0)
+
+
+
+
+
+
