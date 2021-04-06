b23c1714d1343f445a960ff1b7274515b2a5b80f
Fredrik W <xfriko@gmail.com>
2021-04-06 21:53:13 +0200

    Fixing errors

--- Bricks/bricks_main.py
+++ Bricks/bricks_main.py
@@ -33,7 +33,7 @@ BGCOLOR = (32, 32, 32)
 
 def init_lives():
     score = 0
-    lives = 100
+    lives = 3
     return score, lives
 
 
@@ -64,9 +64,9 @@ font3 = pygame.font.SysFont('comicsansms', 30)
 
 
 # Sounds
-main_menu_theme = pygame.mixer.Sound('Bricks/assets/background.mp3')  ##Credit to DonimikBraun @ freesound.org
-game_over_sound = pygame.mixer.Sound('Bricks/assets/game_over.mp3')  ##Credit to Baltiyar13 @ freesound.org
-level_complete_sound = pygame.mixer.Sound('Bricks/assets/level_complete.mp3')  ##Credit to ProjectsU012 @ freesound.org
+main_menu_theme = pygame.mixer.Sound('Bricks/assets/background.wav')  ##Credit to DonimikBraun @ freesound.org
+game_over_sound = pygame.mixer.Sound('Bricks/assets/game_over.wav')  ##Credit to Baltiyar13 @ freesound.org
+level_complete_sound = pygame.mixer.Sound('Bricks/assets/level_complete.wav')  ##Credit to ProjectsU012 @ freesound.org
 
 
 def draw_loading(screen):
